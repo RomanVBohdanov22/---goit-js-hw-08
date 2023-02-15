8c7614c081f5a8294a804d53ca8b3b67f4a22797
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2023-02-15 23:29:40 +0200

    We did it! Task 01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,40 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const galleryMurkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li>
+    <a class="gallery__item" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+        />
+    </a>
+    </li>
+`;
+  })
+    .join('');
+  
+gallery.insertAdjacentHTML('afterbegin', galleryMurkup);
+
+console.log(gallery);
+
+var lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250, captionsData: "alt",}); 
+
+//var lightbox = new SimpleLightbox('.gallery a'); //, { captionDelay: 250, captionsData: "alt",});
+
+/*lightbox.on('show.simplelightbox', function () {
+	// Do something…
+});*/
+
